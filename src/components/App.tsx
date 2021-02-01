496294c6d53fd13ac5be6c1925fac3fc7dc0b2b5
ajaykadus <ajay.kadus@gmail.com>
2021-01-31 16:31:24 -0800

    Fixed eslint error with react

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,10 @@
 import * as React from "react";
-import styled from "styled-components";
-
-export interface IAppProps {}
+import styled, { StyledComponent } from "styled-components";
 
 const Heading = styled.h1<{ active?: boolean }>`
   color: ${(props) => (props?.active ? "red" : "blue")};
 `;
 
-export default function IApp(props: IAppProps) {
-  return <Heading active={true}>Stocks Trading with 6th Sense</Heading>;
+export default function IApp(): any {
+  return <Heading active>Stocks Trading with 6th Sense</Heading>;
 }
