d195bbb94a2c6b92138ae5b894edc3f1033cc3b9
ajaykadus <ajay.kadus@gmail.com>
2021-03-04 21:46:47 -0800

    Added compoent for stock list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,57 @@
 import React, { useState, useEffect } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Link from "@material-ui/core/Link";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
 import StockService from "../Services/Stock/StockService";
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
+const useStyles = makeStyles((theme) => ({
+  "@global": {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: "none",
+    },
   },
-});
+  appBar: {
+    borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: "wrap",
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+  cardHeader: {
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
+  },
+  cardPricing: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "baseline",
+    marginBottom: theme.spacing(2),
+  },
+}));
 
-export default function IApp(): any {
+export default () => {
   const [stocks, setStocks] = useState([]);
   const classes = useStyles();
 
@@ -28,31 +64,111 @@ export default function IApp(): any {
   });
 
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Company Name</TableCell>
-            <TableCell align="right">Ticker Symbol</TableCell>
-            <TableCell align="right">Last Price</TableCell>
-            <TableCell align="right">Predicted Price</TableCell>
-            <TableCell align="right">Notes</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
+    <>
+      <CssBaseline />
+      <AppBar
+        position="static"
+        color="default"
+        elevation={0}
+        className={classes.appBar}
+      >
+        <Toolbar className={classes.toolbar}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.toolbarTitle}
+          >
+            Stocks 6th Sense
+          </Typography>
+          <nav>
+            <Link
+              variant="button"
+              color="textPrimary"
+              href="http://test.com"
+              className={classes.link}
+            >
+              Features
+            </Link>
+            <Link
+              variant="button"
+              color="textPrimary"
+              href="http://test.com"
+              className={classes.link}
+            >
+              Enterprise
+            </Link>
+            <Link
+              variant="button"
+              color="textPrimary"
+              href="http://test.com"
+              className={classes.link}
+            >
+              Support
+            </Link>
+          </nav>
+          <Button
+            href="#"
+            color="primary"
+            variant="outlined"
+            className={classes.link}
+          >
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm" component="main" className={classes.heroContent}>
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Stocks List
+        </Typography>
+      </Container>
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-end">
           {stocks.map((stock) => (
-            <TableRow key={stock.tickerSymbol}>
-              <TableCell component="th" scope="row">
-                {stock.company}
-              </TableCell>
-              <TableCell align="right">{stock.tickerSymbol}</TableCell>
-              <TableCell align="right">{stock.lastPrice}</TableCell>
-              <TableCell align="right">{stock.predictedPrice}</TableCell>
-              <TableCell align="right">None</TableCell>
-            </TableRow>
+            <Grid item key={stock.company} xs={12} sm={2} md={4}>
+              <Card>
+                <CardHeader
+                  title={stock.company}
+                  subheader={stock.subheader}
+                  titleTypographyProps={{ align: "center" }}
+                  subheaderTypographyProps={{ align: "center" }}
+                  action={null}
+                  className={classes.cardHeader}
+                />
+                <CardContent>
+                  <div className={classes.cardPricing}>
+                    <Typography component="h2" variant="h3" color="textPrimary">
+                      $&nbsp;
+                      {stock.lastPrice}
+                    </Typography>
+                  </div>
+                  <ul>
+                    <Typography
+                      component="li"
+                      variant="subtitle1"
+                      align="center"
+                      key="Suggestion"
+                    >
+                      {stock.tickerSymbol}
+                    </Typography>
+                  </ul>
+                </CardContent>
+                <CardActions>
+                  <Button fullWidth color="primary">
+                    Follow
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
           ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+        </Grid>
+      </Container>
+    </>
   );
-}
+};
